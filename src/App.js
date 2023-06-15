070d45696740e6107786170e4edb67e5dc4097fa
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-15 14:23:54 +0300

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout';
+
+import { MainPage } from './pages/MainPage';
+import { PostsPage } from './pages/PostsPage';
+import { PostPage } from './pages/PostPage';
+import { AddPostPage } from './pages/AddPostPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { LoginPage } from './pages/LoginPage';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/posts" element={<PostsPage />}></Route>
+        <Route path="/:id" element={<PostPage />}></Route>
+        <Route path="/new_post" element={<AddPostPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
