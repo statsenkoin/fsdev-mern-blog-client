41d86a1150863c497cc83bd04eb9b5eb53fde0dc
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-15 21:03:01 +0300

    Create common.css, variables.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './assets/styles/common.css';
+import './assets/styles/variables.css';
 
-import { App } from 'components';
+import { App } from './App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
